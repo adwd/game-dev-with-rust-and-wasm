418fc1f376f08b47c436d4394f896a3f841e99ba
adwd <ecdt.mn@gmail.com>
2023-07-21 23:19:50 +0900

    fill random color

--- ch01/walk-the-dog/src/lib.rs
+++ ch01/walk-the-dog/src/lib.rs
@@ -1,3 +1,4 @@
+use rand::{thread_rng, Rng};
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
@@ -24,13 +25,67 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    context.move_to(300.0, 0.0);
-    context.line_to(0.0, 600.0);
-    context.line_to(600.0, 600.0);
-    context.line_to(300.0, 0.0);
+    sierpinski(
+        &context,
+        [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
+        (0, 255, 0),
+        5,
+    );
+
+    Ok(())
+}
+
+fn sierpinski(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+    depth: u8,
+) {
+    draw_triangle(&context, points, color);
+    let mut rng = thread_rng();
+
+    let next_color = (
+        rng.gen_range(0..255),
+        rng.gen_range(0..255),
+        rng.gen_range(0..255),
+    );
+
+    if depth > 0 {
+        let [top, left, right] = points;
+        let left_mid = midpoint(top, left);
+        let right_mid = midpoint(top, right);
+        let bottom_mid = midpoint(left, right);
+
+        sierpinski(context, [top, left_mid, right_mid], next_color, depth - 1);
+        sierpinski(context, [left_mid, left, bottom_mid], next_color, depth - 1);
+        sierpinski(
+            context,
+            [right_mid, bottom_mid, right],
+            next_color,
+            depth - 1,
+        );
+    }
+}
+
+fn midpoint(p1: (f64, f64), p2: (f64, f64)) -> (f64, f64) {
+    ((p1.0 + p2.0) / 2.0, (p1.1 + p2.1) / 2.0)
+}
+
+fn draw_triangle(
+    context: &web_sys::CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+    color: (u8, u8, u8),
+) {
+    let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
+    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
+
+    let [top, left, right] = points;
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
     context.close_path();
     context.stroke();
     context.fill();
-
-    Ok(())
 }
