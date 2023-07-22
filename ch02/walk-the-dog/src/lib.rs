cda763d57feb85e556d71c36052885b7421e1af6
adwd <ecdt.mn@gmail.com>
2023-07-22 09:54:22 +0900

    add error callback

--- ch02/walk-the-dog/src/lib.rs
+++ ch02/walk-the-dog/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{rc::Rc, sync::Mutex};
+
 use rand::{thread_rng, Rng};
 use wasm_bindgen::prelude::*;
 use web_sys::console;
@@ -26,22 +28,31 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
         let image = web_sys::HtmlImageElement::new().unwrap();
 
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
         let callback = Closure::once(move || {
-            web_sys::console::log_1(&JsValue::from_str("loaded"));
-            success_tx.send(()).unwrap();
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
         });
 
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
         image.set_src("Idle (1).png");
 
-        success_rx.await.unwrap();
+        success_rx.await;
 
-        context
-            .draw_image_with_html_image_element(&image, 0.0, 0.0)
-            .unwrap();
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
 
         sierpinski(
             &context,
