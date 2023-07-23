e5aea71c39144a79588a74148a21c8ba3e568be9
Masahiro Nishida <adwd@users.noreply.github.com>
2023-07-23 15:44:29 +0900

    Chapter 3 (#2)
    * add GameLoop
    
    * use keyboard event to update game state
    
    * press arrow key to move rhb
    
    * fix CI
    
    * fix
    
    * test clippy CI
    
    * fix
    
    * fix
    
    * fix
    
    * remove unused deps

--- ch03/walk-the-dog/src/lib.rs
+++ ch03/walk-the-dog/src/lib.rs
@@ -1,30 +1,13 @@
 use anyhow::Result;
-use serde::Deserialize;
-use std::collections::HashMap;
+use engine::GameLoop;
+use game::WalkTheDog;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
 #[macro_use]
 mod browser;
 mod engine;
-
-#[derive(Deserialize, Debug)]
-struct Sheet {
-    frames: HashMap<String, Cell>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Cell {
-    frame: Rect,
-}
-
-#[derive(Deserialize, Debug)]
-struct Rect {
-    x: u16,
-    y: u16,
-    w: u16,
-    h: u16,
-}
+mod game;
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -34,51 +17,12 @@ pub fn main_js() -> Result<(), JsValue> {
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
 
-    let context = browser::context().expect("Could not get browser context");
-
     browser::spawn_local(async move {
-        let json = browser::fetch_json("rhb.json")
+        let game = WalkTheDog::new();
+
+        GameLoop::start(game)
             .await
-            .expect("Could not fetch rhb.json");
-
-        let sheet: Sheet = serde_wasm_bindgen::from_value(json)
-            .expect("Could not convert rhb.json into a Sheet struct");
-
-        let image = engine::load_image("rhb.png")
-            .await
-            .expect("Could not load rhb.png");
-
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
-            context
-                .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                    &image,
-                    sprite.frame.x.into(),
-                    sprite.frame.y.into(),
-                    sprite.frame.w.into(),
-                    sprite.frame.h.into(),
-                    300.0,
-                    300.0,
-                    sprite.frame.w.into(),
-                    sprite.frame.h.into(),
-                )
-                .unwrap();
-        }) as Box<dyn FnMut()>);
-
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                interval_callback.as_ref().unchecked_ref(),
-                50,
-            )
-            .unwrap();
-
-        interval_callback.forget();
+            .expect("Could not start game loop");
     });
 
     Ok(())
